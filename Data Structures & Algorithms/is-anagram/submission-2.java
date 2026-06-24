6f1f1a2962314d8c3a176729fd32d1a4bb0ff73f
Meena-S-work <meena.s.officialwork@gmail.com>
2026-06-24 15:43:22 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        // char[] ssort = s.toCharArray();
+        // char[] tsort = t.toCharArray();
+        // Arrays.sort(ssort);
+        // Arrays.sort(tsort);
+
+        // return Arrays.equals(ssort,tsort);
+
+        
+
+        // HashMap<Character,Integer> counts = new HashMap<>();
+        // HashMap<Character,Integer> countt = new HashMap<>();
+
+        // for(int i =0;i<s.length();i++){
+        //     counts.put(s.charAt(i),counts.getOrDefault(s.charAt(i),0)+1);
+        //     countt.put(t.charAt(i),countt.getOrDefault(t.charAt(i),0)+1);
+        // }
+        // return counts.equals(countt);
+
+        int[] count = new int[26];
+        for(int i =0;i<s.length();i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int val : count){
+            if(val != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
