6589e43e23aa09d232a0aa6c8efd096d12bfe46e
Meena-S-work <meena.s.officialwork@gmail.com>
2026-07-27 13:48:04 +0530

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for(int i =0;i<n/2;i++){
+            char temp = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = temp;
+        }
+
+        // int right =n-1;
+        // int left = 0;
+        // while(left <= right){
+        //     char temp = s[left];
+        //     s[left] = s[right];
+        //     s[right] = temp;
+        //     left++;right--;
+        // }
+    }
+}
